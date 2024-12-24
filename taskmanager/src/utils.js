8a17ad4657d6db3d3b806cf462ec4446f9df73ce
codeXrishi <152185275+codeXrishi@users.noreply.github.com>
2024-12-24 21:33:15 +0530

    Update utils.js

--- taskmanager/src/utils.js
+++ taskmanager/src/utils.js
@@ -3,4 +3,4 @@ import {toast} from 'react-toastify';
 export const notify = (message,type)=>{
     toast[type](message);
 }
-export const API_URL = 'http://localhost:5000';
+export const API_URL = 'https://mern-task-manager-deploy-api.vercel.app';
